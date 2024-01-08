227b850e855ca33c4cbf7014b5b4a7eccbdb2ac9
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-08 17:22:04 +0330

    add exception for initialize jdbcconnection on AuthorRepository class

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -1,5 +1,11 @@
+import java.sql.SQLException;
+
 public class AuthorRepository {
     private final Jdbcconnection jdbcconnection=new Jdbcconnection();
+
+    public AuthorRepository() throws SQLException {
+    }
+
     public void save (Author author){
 
     }
