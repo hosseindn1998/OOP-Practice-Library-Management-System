198886900be3fc3e643bd718f912f667a66df5ae
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-08 17:14:59 +0330

    Create connection initialize in jdbcconnection class

--- src/main/java/Jdbcconnection.java
+++ src/main/java/Jdbcconnection.java
@@ -1,2 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Jdbcconnection {
+    private final Connection connection=
+            DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","#HDn1998");
+
+
 }
