a501af4d74d926180885ec3a1c314884b6888631
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 11:23:20 +0330

    add save author method codes in AuthorRepository class

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -12,12 +12,12 @@ public class AuthorRepository {
 
     public void save (Author author) throws SQLException {
         Connection connection= jdbcconnection.getConnection();
-        String saveAuthor="INSERT INTO author(first_name,last_name,age,book)VALUES(?,?,?,?);";
+        String saveAuthor="INSERT INTO author(first_name,last_name,age,book)VALUES(?,?,?);";
         PreparedStatement preparedStatement=connection.prepareStatement(saveAuthor);
         preparedStatement.setString(1,author.getFirstName());
         preparedStatement.setString(2,author.getLastName());
         preparedStatement.setInt(3,author.getAge());
-        preparedStatement.setArray(4, connection.createArrayOf("Book", ));
+//        preparedStatement.setInt(4,author.getBook());
         preparedStatement.executeUpdate();
     }
 
