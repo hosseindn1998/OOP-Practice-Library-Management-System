78d9666d265200f8f895e821a14242b21c8f29d1
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 21:48:54 +0330

    remove book[] in constructor in author

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -10,7 +10,7 @@ public class Author {
 
     }
 
-    public Author(String firstName, String lastName, int age, int[] book) {
+    public Author(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
