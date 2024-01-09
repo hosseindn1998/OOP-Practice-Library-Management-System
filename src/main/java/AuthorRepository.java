b2517739618376a1a6439af83d786eb3c7913540
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 09:51:32 +0330

    Create author to

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -1,4 +1,8 @@
+import java.awt.print.Book;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 public class AuthorRepository {
     private final Jdbcconnection jdbcconnection=new Jdbcconnection();
@@ -6,8 +10,15 @@ public class AuthorRepository {
     public AuthorRepository() throws SQLException {
     }
 
-    public void save (Author author){
-
+    public void save (Author author) throws SQLException {
+        Connection connection= jdbcconnection.getConnection();
+        String saveAuthor="INSERT INTO author(first_name,last_name,age,book)VALUES(?,?,?,?);";
+        PreparedStatement preparedStatement=connection.prepareStatement(saveAuthor);
+        preparedStatement.setString(1,author.getFirstName());
+        preparedStatement.setString(2,author.getLastName());
+        preparedStatement.setInt(3,author.getAge());
+        preparedStatement.setArray(4, connection.createArrayOf("Book", ));
+        preparedStatement.executeUpdate();
     }
 
 }
