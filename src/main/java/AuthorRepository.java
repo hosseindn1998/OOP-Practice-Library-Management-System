b440447cec3f768269cf1e9af492ed22a5cab47e
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 20:48:11 +0330

    add authorload method codes

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -1,6 +1,7 @@
 import java.awt.print.Book;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 
@@ -20,5 +21,12 @@ public class AuthorRepository {
 //        preparedStatement.setInt(4,author.getBook());
         preparedStatement.executeUpdate();
     }
+    public void authorload (int authorId) throws SQLException {
+        Connection connection= jdbcconnection.getConnection();
+        String loadAuthor="SELECT * FROM author WHERE author_id=VALUES(?);";
+        PreparedStatement preparedStatement=connection.prepareStatement(loadAuthor);
+        preparedStatement.setInt(1,authorId);
+        ResultSet resultSet= preparedStatement.executeQuery();
+        System.out.println(resultSet);
 
-}
+}}
