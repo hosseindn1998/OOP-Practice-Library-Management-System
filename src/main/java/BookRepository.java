b09678b39d953bc8dd40898d3318905ad952fdb0
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 23:03:30 +0330

    book load by id method codes in BookRepository

--- src/main/java/BookRepository.java
+++ src/main/java/BookRepository.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BookRepository implements CRUD {
@@ -15,7 +16,24 @@ public void save(Book book) throws SQLException {
         preparedStatement.setString(1,book.getTitle());
         preparedStatement.setInt(2,book.getYear());
         preparedStatement.setInt(3,book.getAuthorID());
-
         preparedStatement.executeUpdate();
+    }
+    public Book load (int bookId) throws SQLException {
+        Connection connection = jdbcconnection.getConnection();
+        String loadBook = "SELECT * FROM book WHERE book_id=VALUES(?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadBook);
+        preparedStatement.setInt(1, bookId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()) {
+            int bookID=bookId;
+            String title=resultSet.getString("title");
+            int year=resultSet.getInt("year");
+            int authorID=resultSet.getInt("autherID");
+            return new Book(title,year,authorID);
+        }else{
+            System.out.println("author not found please register now");
+            return new Book("not found",-1,-1);
+        }
+
     }
 }
