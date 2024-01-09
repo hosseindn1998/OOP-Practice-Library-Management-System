3bf4db4e8566b38d66c92f69a6723227fa268dbf
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 11:54:10 +0330

    add Save method codes in BookRepository

--- src/main/java/BookRepository.java
+++ src/main/java/BookRepository.java
@@ -1,8 +1,21 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class BookRepository {
+public class BookRepository implements CRUD {
     private final Jdbcconnection jdbcconnection=new Jdbcconnection();
 
     public BookRepository() throws SQLException {
+    }
+@Override
+public void save(Book book) throws SQLException {
+        Connection connection= jdbcconnection.getConnection();
+        String saveBook="INSERT INTO book(title,year,authorID)VALUES(?,?,?);";
+        PreparedStatement preparedStatement=connection.prepareStatement(saveBook);
+        preparedStatement.setString(1,book.getTitle());
+        preparedStatement.setInt(2,book.getYear());
+        preparedStatement.setInt(3,book.getAuthorID());
+
+        preparedStatement.executeUpdate();
     }
 }
