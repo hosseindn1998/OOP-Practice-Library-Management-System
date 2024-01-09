98210e36da7314d69f6a62291031754bc7d80c97
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 21:31:01 +0330

    add authorload method codes in AuthorRepository

--- src/main/java/AuthorRepository.java
+++ src/main/java/AuthorRepository.java
@@ -6,27 +6,29 @@ import java.sql.SQLException;
 import java.util.Arrays;
 
 public class AuthorRepository {
-    private final Jdbcconnection jdbcconnection=new Jdbcconnection();
+    private final Jdbcconnection jdbcconnection = new Jdbcconnection();
 
     public AuthorRepository() throws SQLException {
     }
 
-    public void save (Author author) throws SQLException {
-        Connection connection= jdbcconnection.getConnection();
-        String saveAuthor="INSERT INTO author(first_name,last_name,age,book)VALUES(?,?,?);";
-        PreparedStatement preparedStatement=connection.prepareStatement(saveAuthor);
-        preparedStatement.setString(1,author.getFirstName());
-        preparedStatement.setString(2,author.getLastName());
-        preparedStatement.setInt(3,author.getAge());
+    public void save(Author author) throws SQLException {
+        Connection connection = jdbcconnection.getConnection();
+        String saveAuthor = "INSERT INTO author(first_name,last_name,age,book)VALUES(?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(saveAuthor);
+        preparedStatement.setString(1, author.getFirstName());
+        preparedStatement.setString(2, author.getLastName());
+        preparedStatement.setInt(3, author.getAge());
 //        preparedStatement.setInt(4,author.getBook());
         preparedStatement.executeUpdate();
     }
-    public void authorload (int authorId) throws SQLException {
-        Connection connection= jdbcconnection.getConnection();
-        String loadAuthor="SELECT * FROM author WHERE author_id=VALUES(?);";
-        PreparedStatement preparedStatement=connection.prepareStatement(loadAuthor);
-        preparedStatement.setInt(1,authorId);
-        ResultSet resultSet= preparedStatement.executeQuery();
+
+    public void authorload(int authorId) throws SQLException {
+        Connection connection = jdbcconnection.getConnection();
+        String loadAuthor = "SELECT * FROM author WHERE author_id=VALUES(?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadAuthor);
+        preparedStatement.setInt(1, authorId);
+        ResultSet resultSet = preparedStatement.executeQuery();
         System.out.println(resultSet);
 
-}}
+    }
+}
