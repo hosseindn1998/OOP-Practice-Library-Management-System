51b7c4f37a38a18e336a995dcb10445b6128e8d9
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-09 08:57:40 +0330

    fix book datatuype in create table in author and rename auther to author

--- src/main/java/table.sql
+++ src/main/java/table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS auther(
+CREATE TABLE IF NOT EXISTS author(
     id SERIAL PRIMARY KEY ,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     age INTEGER,
-    book VARCHAR[]
+    book Book[]
     );
 CREATE TABLE IF NOT EXISTS book(
     id SERIAL PRIMARY KEY,
